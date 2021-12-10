e6d81d1ab04b010b496bc7cb8bffdc75421071ce
Kondr11 <s.alexk1201@gmail.com>
2021-12-10 10:58:47 +0300

    old

---
+++ QR_mb/QR_mb/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace QR_mb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LOGINBOX = new System.Windows.Forms.TextBox();
+            this.PASSBOX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RECIPIENT = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.LOG = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Decode code";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(55, 238);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Browse a local image\r\n";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(77, 278);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Donwload";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(616, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(211, 146);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // LOGINBOX
+            // 
+            this.LOGINBOX.Location = new System.Drawing.Point(257, 22);
+            this.LOGINBOX.Name = "LOGINBOX";
+            this.LOGINBOX.Size = new System.Drawing.Size(94, 20);
+            this.LOGINBOX.TabIndex = 6;
+            // 
+            // PASSBOX
+            // 
+            this.PASSBOX.Location = new System.Drawing.Point(257, 73);
+            this.PASSBOX.Name = "PASSBOX";
+            this.PASSBOX.Size = new System.Drawing.Size(100, 20);
+            this.PASSBOX.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(390, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(375, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "кому отправить";
+            // 
+            // RECIPIENT
+            // 
+            this.RECIPIENT.Location = new System.Drawing.Point(257, 121);
+            this.RECIPIENT.Name = "RECIPIENT";
+            this.RECIPIENT.Size = new System.Drawing.Size(100, 20);
+            this.RECIPIENT.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(257, 177);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "отправить в вк";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // LOG
+            // 
+            this.LOG.Location = new System.Drawing.Point(12, 307);
+            this.LOG.Multiline = true;
+            this.LOG.Name = "LOG";
+            this.LOG.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.LOG.Size = new System.Drawing.Size(211, 131);
+            this.LOG.TabIndex = 13;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(470, 76);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "скрыть пароль";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(257, 211);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(393, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "цвет фона";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(393, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "цвет узора";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(257, 238);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 17;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(260, 278);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "перерисовать";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.LOG);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RECIPIENT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PASSBOX);
+            this.Controls.Add(this.LOGINBOX);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox LOGINBOX;
+        private System.Windows.Forms.TextBox PASSBOX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox RECIPIENT;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox LOG;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ QR_mb/QR_mb/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing.Common;
+using ZXing;
+using ZXing.QrCode;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using ZXing.Client;
+
+namespace QR_mb
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver web;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        QrCodeEncodingOptions options = new QrCodeEncodingOptions();
+        Bitmap bmp1;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 250,
+                Height = 250,
+            };
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+            comboBox1.Items.AddRange(Enum.GetNames(typeof(KnownColor)));
+            comboBox1.Text = KnownColor.White.ToString();
+            comboBox2.Items.AddRange(Enum.GetNames(typeof(KnownColor)));
+            comboBox2.Text = KnownColor.Black.ToString();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(textBox1.Text) || String.IsNullOrEmpty(textBox1.Text))
+            {
+                pictureBox1.Image = null;
+                MessageBox.Show("Text not found", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var qr = new ZXing.BarcodeWriter();
+                qr.Options = options;
+                qr.Format = ZXing.BarcodeFormat.QR_CODE;
+                var result = new Bitmap(qr.Write(textBox1.Text.Trim()));
+                pictureBox1.Image = result;
+                textBox1.Clear();
+                Clipboard.SetImage(pictureBox1.Image);
+                bmp1 = new Bitmap(pictureBox1.Image);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                BarcodeReader reader = new BarcodeReader { AutoRotate = true, TryInverted = true };
+                Result result = reader.Decode(bitmap);
+                string decoded = result.ToString().Trim();
+                textBox1.Text = decoded;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Image not found", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var qr = new ZXing.BarcodeWriter();
+                qr.Options = options;
+                qr.Format = ZXing.BarcodeFormat.QR_CODE;
+                pictureBox1.ImageLocation = open.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Image not found", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                SaveFileDialog save = new SaveFileDialog();
+                save.CreatePrompt = true;
+                save.OverwritePrompt = true;
+                save.FileName = "QR";
+                save.Filter = "PNG|*.png|JPEG|*.jpg|BMP|*.bmp|GIF|*.gif";
+                if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(save.FileName);
+                    save.InitialDirectory = Environment.GetFolderPath
+                                (Environment.SpecialFolder.Desktop);
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string LOGIN = LOGINBOX.Text;
+            string PASS = PASSBOX.Text;
+            web = new ChromeDriver();
+            web.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(40);
+            try
+            {
+                try
+                {
+                    web.Navigate().GoToUrl("https://vk.com");
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось перейти по URL" + Environment.NewLine);
+                }
+
+                try
+                {
+                    web.FindElement(By.XPath("//input[@id='index_email']")).SendKeys(LOGIN);
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось ввести логин" + Environment.NewLine);
+                }
+
+                try
+                {
+                    web.FindElement(By.XPath("//input[@id='index_pass']")).SendKeys(PASS);
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось ввести пароль" + Environment.NewLine);
+                }
+                try
+                {
+                    web.FindElement(By.XPath("//button[@id='index_login_button']")).Click();
+                    web.FindElement(By.XPath("//li[@id='l_msg']")).Click();
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось нажать кнопку ВОЙТИ" + Environment.NewLine);
+                }
+
+                try 
+                {
+                    web.FindElement(By.XPath("//li[@id='l_msg']/a[@class='left_row']/span[@class='left_fixer']/span[@class='left_label inl_bl']")).Click();
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось перейти в сообщения" + Environment.NewLine);
+                }
+
+                try
+                {
+                    web.FindElement(By.XPath("//input[@id='im_dialogs_search']")).SendKeys(RECIPIENT.Text); ;
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось ввести получателя" + Environment.NewLine);
+                }
+
+                try
+                {
+                    web.FindElement(By.XPath("//div[@class='nim-dialog--cw']")).Click(); ;
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось перейти в сообщения с получателем" + Environment.NewLine);
+                }
+
+                try
+                {
+                    web.FindElement(By.XPath("//div[@id='im_editable0']")).SendKeys(OpenQA.Selenium.Keys.Control + "v");
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось вставить картинку" + Environment.NewLine);
+                }
+
+                try
+                {
+                    web.FindElement(By.XPath("//button[@class='im-send-btn im-chat-input--send _im_send im-send-btn_send']")).Click(); ;
+                }
+                catch (Exception)
+                {
+                    LOG.AppendText("Ошибка. Не удалось отправить сообщение" + Environment.NewLine);
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            web.Dispose();
+            web.Quit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!PASSBOX.UseSystemPasswordChar)
+                PASSBOX.UseSystemPasswordChar = true;
+            else
+                PASSBOX.UseSystemPasswordChar = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            for (int i = 0; i < options.Height; i++)
+            {
+                for (int j = 0; j < options.Width; j++)
+                {
+                    if (bmp1.GetPixel(i, j).ToArgb() == Color.White.ToArgb())
+                    {
+                        bmp.SetPixel(i, j, Color.FromName(comboBox1.SelectedItem.ToString()));
+                    }
+                    else
+                    {
+                        if (bmp1.GetPixel(i, j).ToArgb() == Color.Black.ToArgb())
+                            bmp.SetPixel(i, j, Color.FromName(comboBox2.SelectedItem.ToString()));
+                    }
+                }
+            }
+            pictureBox1.Image = bmp;
+            Clipboard.SetImage(pictureBox1.Image);
+        }
+    }
+}
